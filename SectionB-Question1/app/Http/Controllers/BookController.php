c67012f83b10ef143ea79a72e1360a432e94cad5
bankat <bmananaf@gmail.com>
2024-07-29 12:13:28 +0100

    add controllers, models and migrations

--- SectionB-Question1/app/Http/Controllers/BookController.php
+++ SectionB-Question1/app/Http/Controllers/BookController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return Book::orderBy('title', 'ASC')->paginate(25);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'author_id' => 'required|exists:authors,id',
+            'publication_year' => 'required|digits:4|integer|min:1500|max:'.(date('Y')+1),
+            'genre' => 'required|string|max:255',
+        ]);
+
+        return Book::create($request->all());
+    }
+
+    public function show(Book $book)
+    {
+        return $book;
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'author_id' => 'required|exists:authors,id',
+            'publication_year' => 'required|digits:4|integer|min:1500|max:'.(date('Y')+1),
+            'genre' => 'required|string|max:255',
+        ]);
+
+        $book->update($request->all());
+
+        return $book;
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return response()->noContent();
+    }
 }
